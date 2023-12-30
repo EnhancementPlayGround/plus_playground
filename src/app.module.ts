917bf24d88a1721013195805f292455cffb7aaf0
jinsun yoon <yoon.jinsun@aiv.ai>
2023-12-30 15:44:04 +0900

    prisma+graphql setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { ProductModule } from './product/product.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    ProductModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
