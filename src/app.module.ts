ec3e67f929fc54e20a5b659d94b8cfef8847ee82
jinsun yoon <yoon.jinsun@aiv.ai>
2024-01-03 22:58:45 +0900

    logging setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,10 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
@@ -6,9 +12,14 @@ import { ProductModule } from './product/product.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { HttpLoggerMiddleware } from './middleware/http.logger.middleware';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
     HealthModule,
     ProductModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -16,9 +27,18 @@ import { join } from 'path';
       playground: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
+      introspection: true,
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(HttpLoggerMiddleware)
+      .exclude('/')
+      .exclude('/health')
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
